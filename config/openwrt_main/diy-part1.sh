72e89cadec153c5174a81616c1b6f74e166a649e
waltermanpro <164705679+waltermanpro@users.noreply.github.com>
2026-08-19 09:19:51 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -8,7 +8,7 @@
 
 # Add a custom feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git OpenClash https://github.com/vernesong/OpenClash' feeds.conf.default
 # Remove unnecessary packages
 # rm -rf package/utils/{ucode,fbtest}
 
