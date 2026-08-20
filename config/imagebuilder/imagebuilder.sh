98b8c984669530c1e86e2d5f193f7fc0b1bcdb50
waltermanpro <164705679+waltermanpro@users.noreply.github.com>
2026-08-20 15:28:47 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -231,7 +231,7 @@ rebuild_firmware() {
         luci-mod-status luci-mod-system luci-proto-3g luci-proto-ipip luci-proto-ipv6 \
         luci-proto-ncm luci-proto-openconnect luci-proto-ppp luci-proto-qmi luci-proto-relay \
         \
-        luci-app-amlogic luci-i18n-amlogic-zh-cn \
+        luci-app-amlogic luci-i18n-amlogic-zh-cn luci-app-openclash \
         \
         ${config_list} \
         "
